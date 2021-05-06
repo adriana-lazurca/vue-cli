40850d63d9c262b1ba72e4004aa320c254d81c39
Adriana Lazurca <adriana.lazurca@gmail.com>
2021-05-06 16:24:47 +0200

    add files

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import HomeLink from "./components/HomeLink.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomeLink
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <nav>
+      <HomeLink />
+      <a href="/about">À propos</a>
+      <a href="/contact">Contact</a>
+    </nav>
+    <p>Bienvenue sur notre page d'<HomeLink /></p>
+  </div>
+</template>
+
+<style lang="scss">
+/*$app-color: green;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $app-color;
+  margin-top: 60px;
+
+  img {
+    width: 50px;
+  }
+}*/
+</style>
