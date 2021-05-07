7d88a6a2da9d56dfb1603e972269bee385314bca
Adriana Lazurca <adriana.lazurca@gmail.com>
2021-05-07 17:20:46 +0200

    add contact view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    
-  </div>
+	<div id="app" class="app">
+		<h1>{{ restaurantName }}</h1>
+		<p class="description">
+			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+			est difficile de s'arrêter.
+		</p>
+
+		<section class="menu">
+			<h2>Menu</h2>
+      <ItemDetail name="croissant" />
+			<MenuItem
+				v-for="item in simpleMenu"
+				:addToShoppingCart="addToShoppingCart"
+				:name="item.name"
+				:image="item.image"
+				:price="item.price"
+				:quantity="item.quantity"
+				:inStock="item.inStock"
+				:key="item.name"
+			/>
+		</section>
+
+		<aside class="shopping-cart">
+			<h2>Panier : {{ shoppingCart }} articles</h2>
+		</aside>
+
+		<footer class="footer">
+			<p>{{ copyright }}</p>
+		</footer>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import MenuItem from "../components/MenuItem"
 export default {
-  name: 'Home',
-  components: {
-    
-  }
+	name: "Home",
+	components: {
+		MenuItem
+	},
+	data() {
+		return {
+			restaurantName: "La belle vue",
+			shoppingCart: 0,
+			simpleMenu: [
+				{
+					name: "Croissant",
+					image: {
+						source: "/images/crossiant.jpg",
+						alt: "Un croissant"
+					},
+					inStock: true,
+					quantity: 1,
+					price: 2.99
+				},
+				{
+					name: "Baguette de pain",
+					image: {
+						source: "/images/french-baguette.jpeg",
+						alt: "Quatre baguettes de pain"
+					},
+					inStock: true,
+					quantity: 1,
+					price: 3.99
+				},
+				{
+					name: "Éclair",
+					image: {
+						source: "/images/eclair.jpg",
+						alt: "Éclair au chocolat"
+					},
+					inStock: false,
+					quantity: 1,
+					price: 4.99
+				}
+			]
+		}
+	},
+	computed: {
+		copyright() {
+			const currentYear = new Date().getFullYear()
+			return `Copyright ${this.restaurantName} ${currentYear}`
+		}
+	},
+	methods: {
+		addToShoppingCart(amount) {
+			this.shoppingCart += amount
+		}
+	}
 }
 </script>
---
+++ src/views/ItemDetail.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "ItemDetail"
+};
+</script>
+
+<template>
+  <div class="menu-item">
+    <div>Item: {{ $route.params.name }}</div>
+  </div>
+</template>
+
+<style></style>
